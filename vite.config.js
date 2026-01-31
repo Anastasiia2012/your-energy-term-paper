ec2cf072ef5a58b2f55df8625707a38f5ff56ee2
Ievgenii Gryshkun <geo@macbook-pro-ievgenii.home>
2026-01-31 19:22:08 +0100

    test

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import { glob } from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 import SortCss from 'postcss-sort-media-queries';
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig(({ command }) => {
   return {
@@ -44,7 +43,6 @@ export default defineConfig(({ command }) => {
       SortCss({
         sort: 'mobile-first',
       }),
-      tailwindcss(),
     ],
   };
 });
