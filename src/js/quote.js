61969b7798661c0a35571a8373b1335e4094a182
Ievgenii Gryshkun <geo@macbook-pro-ievgenii.home>
2026-01-31 19:45:27 +0100

    test

--- src/js/quote.js
+++ src/js/quote.js
@@ -2,7 +2,17 @@ import { fetchData } from "./fetchApi.js";
 import { renderQuote } from "./renderer/quote.js";
 
 const API_URL = "https://your-energy.b.goit.study/api/quote";
+const STORAGE_KEY = "savedQuote";
 
-fetchData(API_URL, null, false)
-  .then((quote) => renderQuote(quote))
-  .catch((error) => console.log(error));
+const savedQuote = localStorage.getItem(STORAGE_KEY);
+
+if (savedQuote) {
+  renderQuote(JSON.parse(savedQuote));
+} else {
+  fetchData(API_URL, null, false)
+    .then((quote) => {
+      localStorage.setItem(STORAGE_KEY, JSON.stringify(quote));
+      renderQuote(quote);
+    })
+    .catch((error) => console.log(error));
+}
