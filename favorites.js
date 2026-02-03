e8e258faff10dbfd2c3112329e30841c055b5281
geo208 <ievgenii.gryshkun@gmail.com>
2026-02-03 12:33:57 +0000

    Deploying to gh-pages from @ Anastasiia2012/your-energy-term-paper@cd888cbefec681da4cec1b7c655646ebee59a078 🚀

--- favorites.js
+++ favorites.js
@@ -1,2 +1,2 @@
-import{g as r,r as s}from"./assets/quote-DvdG4qBH.js";const e=r();e.length!==0&&s({results:e});
+import{g as r,r as s}from"./assets/quote-DoXF9lon.js";const e=r();e.length!==0&&s({results:e});
 //# sourceMappingURL=favorites.js.map
